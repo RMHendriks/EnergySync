cca5bbccf3e02a73f0d7fad8fa38d50808efeab6
xstoofpeer <sukhjin.saran@gmail.com>
2023-06-12 16:10:51 +0200

    cable class added

--- code/classes/cable.py
+++ code/classes/cable.py
@@ -1,5 +1,27 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from code.classes.cell import Cell
+
+import pygame
 
 class Cable():
-    def __init__(self) -> None:
-        #TODO
-        pass
+    def __init__(self, cell: Cell) -> None:
+
+        self.cell = cell
+
+        self.sprite = self.load_sprite()
+
+    def draw(self, window: pygame.surface.Surface) -> None:
+        """ Draw the cable to the screen. """
+
+        img_center = (self.cell.x + self.cell.size // 2,
+                      self.cell.y + self.cell.size // 2)
+        window.blit(self.sprite, img_center)
+
+    def load_sprite(self) -> None:
+        """ Load the Cable sprite. """
+
+        sprite = pygame.image.load("sprites/cable.png")
+        return pygame.transform.scale(sprite, (self.cell.size * 1,
+                                               self.cell.size * 1))
